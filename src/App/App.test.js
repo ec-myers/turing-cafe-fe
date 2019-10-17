ea8cb6d82c0164f49985d505f4266ce8a94b696e
“Elyse <elysemyers1@gmail.com>
2019-10-17 10:57:11 -0600

    Adds test to check for change in state within app component when new reservation is made

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,9 +1,27 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('App', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<App />)
+  });
+
+  it('should update state with a new reservation when addReservation is called', () => {
+    let wrapper = shallow(<App />)
+    const mockReservation = {
+      "id": 1,
+      "name": "Christie",
+      "date": "12/29",
+      "time": "7:00",
+      "number": 12
+    }
+    const expected = [mockReservation];
+
+    expect(wrapper.state('reservations')).toEqual([]);
+    wrapper.instance().addReservation(mockReservation);
+    expect(wrapper.state('reservations')).toEqual(expected);
+  });
 });
