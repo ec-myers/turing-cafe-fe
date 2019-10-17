986ac7c8d5d2877c101bd9e9faf3ab406e0c14ea
“Elyse <elysemyers1@gmail.com>
2019-10-17 10:39:29 -0600

    Adds event simulation test for handleChange method

--- src/Form/Form.test.js
+++ src/Form/Form.test.js
@@ -3,11 +3,25 @@ import { shallow } from 'enzyme';
 import Form from './Form';
 
 describe ('Form', () => {
-  it('should match the snapshot', () => {
+  let wrapper;
+
+  beforeEach(() => {
     const mockAddReservation = jest.fn();
-    let wrapper = shallow(<Form 
+    wrapper = shallow(<Form
       addReservation={mockAddReservation} />);
+  });
+  
+  it('should match the snapshot', () => {
     
     expect(wrapper).toMatchSnapshot();
   });
-})
+
+  it('should update state when handleChange is called', () => {
+    const mockEvent = { target: { name: 'name', value: 'khalid' } };
+    const expected = 'khalid';
+
+    wrapper.instance().handleChange(mockEvent);
+
+    expect(wrapper.state('name')).toEqual(expected);
+  });
+});
