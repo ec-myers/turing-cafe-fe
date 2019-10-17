d6c51eb47a17cd7ed27b194f97653acd3a733396
“Elyse <elysemyers1@gmail.com>
2019-10-17 10:16:43 -0600

    Adds apiCalls file

---
+++ src/apiCalls.js
@@ -0,0 +1,19 @@
+export const getReservations = () => {
+  return fetch('http://localhost:3001/api/v1/reservations	')
+    .then(res => res.json())
+}
+
+export const postReservation = (newReservation) => {
+  const options = {
+    method: 'POST',
+    body: JSON.stringify(newReservation),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  };
+
+  return fetch('http://localhost:3001/api/v1/reservations	', options)
+    .then(response => response.json())
+    .catch(error => console.log(error));
+}
+ 
